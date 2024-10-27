5a659f64f87e16879f3b8911cfb1b2bdd7e36619
AliceRemake <1302393176@qq.com>
2024-10-27 12:34:05 +0800

    [Initial Commit]

---
+++ Fire/Fire.cpp
@@ -0,0 +1,15 @@
+/**
+  ******************************************************************************
+  * @file           : Fire.cpp
+  * @author         : AliceRemake
+  * @brief          : None
+  * @attention      : None
+  * @date           : 24-10-27
+  ******************************************************************************
+  */
+
+
+#include <cstdio>
+#include "Fire.h"
+
+FIRE_API void Print() { printf("Hello Fire Engine"); }
---
+++ sandbox.cpp
@@ -0,0 +1,19 @@
+/**
+  ******************************************************************************
+  * @file           : sandbox.cpp
+  * @author         : AliceRemake
+  * @brief          : None
+  * @attention      : None
+  * @date           : 24-10-27
+  ******************************************************************************
+  */
+
+
+#include "Fire/Fire.h"
+
+int main(int argc, char** argv) {
+
+  Print();
+  
+  return 0;
+}
